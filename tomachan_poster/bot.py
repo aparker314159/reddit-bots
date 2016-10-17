5c4b93b353c99e1939109cd8fcf1888c49de1fe9
Andrew <aparker314159@gmail.com>
2016-10-17 16:04:43 -0400

    Added timer to bot. Now waits until 2 to start posting

--- tomachan_poster/bot.py
+++ tomachan_poster/bot.py
@@ -2,7 +2,7 @@ import praw
 import OAuth2Util
 import feedparser
 from time import sleep
-from datetime import date
+from datetime import datetime
 
 subreddit = "aparkerbotplayground"
 
@@ -14,9 +14,16 @@ user_agent = "A script to post the latest comic from read.tomochan.today by /u/a
 r = praw.Reddit(user_agent)
 o = OAuth2Util.OAuth2Util(r)
 o.refresh()
+while True:
+    hour = datetime.today().hour
+    print(hour)
+    if hour == 2:
+        break
+    sleep(3600)
+o.refresh()
 
 while True:
-    post_title = "Page for " + date.today().strftime("%m/%d/%y")
-    r.submit(subreddit, post_title, url=get_feed_entry().link)
+    post_title = "Page for " + datetime.today().strftime("%m/%d/%y")
+    r.submit(subreddit, post_title, url=get_feed_entry().link, resubmit=True)
     sleep(86400)
     o.refresh()

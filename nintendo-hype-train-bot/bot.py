1e550357db7182936f1bb13ecd52f02eccfe5ff6
Andrew <aparker314159@gmail.com>
2016-10-25 21:25:46 -0400

    Added refresh token

--- nintendo-hype-train-bot/bot.py
+++ nintendo-hype-train-bot/bot.py
@@ -1,10 +1,20 @@
 import praw
 import OAuth2Util
+from time import sleep
 
-subname = "aparkerbotplayground"
+subname = "askreddit"
+finished = set()
 
 r = praw.Reddit(user_agent = "Nintendo Switch Hype Train v0.1 by /u/aparker314159")
 o = OAuth2Util.OAuth2Util(r)
 
 while True:
     subreddit = r.get_subreddit(subname)
+    comments = subreddit.get_comments(limit=200)
+    for c in comments:
+        if c in finished:
+            break
+        print(c.body)
+        finished.update([c])
+    sleep(1800)
+    o.refresh()

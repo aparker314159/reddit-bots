f39cf3a5be0f08adcc4b4aabae4dd686af9290eb
Andrew <aparker314159@gmail.com>
2016-10-26 11:47:32 -0400

    Now tests comments for substring, and made message use a generator.

--- nintendo-hype-train-bot/bot.py
+++ nintendo-hype-train-bot/bot.py
@@ -2,13 +2,24 @@ import praw
 import OAuth2Util
 from time import sleep
 
-subname = "aparkerbotplayground"
-finished = set()
-
 r = praw.Reddit(user_agent = "Nintendo Switch Hype Train v0.1 by /u/aparker314159")
 o = OAuth2Util.OAuth2Util(r)
 
+subname = "aparkerbotplayground"
+finished = set()
 onboard = 0
+
+def genmessage(count):
+    yield """
+#ALL ABOARD THE SWITCH HYPE TRAIN!
+#[CHOO CHOO!](http://imgur.com/y9igYes)"""
++ str(count) + """ people onboard the hype train!
+^___________________________________________________________________
+
+^This ^is ^a ^bot, ^and ^this ^action ^was ^performed ^automatically.
+
+^Please ^contact ^[\/u\/aparker314159](https://reddit.com/user/aparker314159) ^for ^any ^questions ^or ^comments.
+"""
 
 while True:
     subreddit = r.get_subreddit(subname)
@@ -16,19 +27,10 @@ while True:
     for c in comments:
         if c in finished:
             break
-        onboard = onboard + 1
-        message = """
-#ALL ABOARD THE SWITCH HYPE TRAIN!
-#[CHOO CHOO!](http://imgur.com/y9igYes)
-
-""" + str(onboard) + """ people onboard the hype train!
-
-^___________________________________________________________________
-
-^This ^is ^a ^bot, ^and ^this ^action ^was ^performed ^automatically.
-^Please ^contact ^/u/aparker314159 ^for ^any ^questions ^or ^comments.
-        """
-        c.reply(message)
+        if "hype train" in (c.body).lower():
+            onboard = onboard + 1
+            message = next(genmessage(onboard))
+            c.reply(message)
         finished.update([c])
     sleep(900)
     o.refresh()

303d5f0d246f544efaaceb8ed76280ed2d00205a
Andrew <aparker314159@gmail.com>
2016-10-26 11:23:08 -0400

    Now replies to comments

--- nintendo-hype-train-bot/bot.py
+++ nintendo-hype-train-bot/bot.py
@@ -2,11 +2,13 @@ import praw
 import OAuth2Util
 from time import sleep
 
-subname = "askreddit"
+subname = "aparkerbotplayground"
 finished = set()
 
 r = praw.Reddit(user_agent = "Nintendo Switch Hype Train v0.1 by /u/aparker314159")
 o = OAuth2Util.OAuth2Util(r)
+
+onboard = 0
 
 while True:
     subreddit = r.get_subreddit(subname)
@@ -14,7 +16,17 @@ while True:
     for c in comments:
         if c in finished:
             break
-        print(c.body)
+        onboard = onboard + 1
+        message = """
+        #ALL ABOARD THE SWITCH HYPE TRAIN!
+        #[CHOO CHOO!](http://imgur.com/y9igYes)
+        """ + str(onboard) + """people onboard the hype train!
+
+        ^________________________________
+
+        ^This ^is ^a ^bot, ^and ^this ^action ^was ^performed ^automatically. ^Please ^contact ^/u/aparker314159 ^for ^any ^questions ^or ^comments.
+        """
+        c.reply(message)
         finished.update([c])
-    sleep(1800)
+    sleep(900)
     o.refresh()

ffa8d07a92522a31581ee86e01b11cff2ddd18c7
Andrew <aparker314159@gmail.com>
2016-10-26 13:51:43 -0400

    Made bot not reply to self.

--- nintendo-hype-train-bot/bot.py
+++ nintendo-hype-train-bot/bot.py
@@ -2,7 +2,7 @@ import praw
 import OAuth2Util
 from time import sleep
 
-r = praw.Reddit(user_agent = "Nintendo Switch Hype Train v0.1 by /u/aparker314159")
+r = praw.Reddit(user_agent = "Nintendo Switch Hype Train v1.0 by /u/aparker314159")
 o = OAuth2Util.OAuth2Util(r)
 
 subname = "aparkerbotplayground"
@@ -10,7 +10,7 @@ finished = set()
 onboard = 0
 
 def genmessage(count):
-    yield "#ALL ABOARD THE SWITCH HYPE TRAIN!\n#[CHOO CHOO!](http://imgur.com/y9igYes)\n" + str(onboard) + " people onboard the hype train!\n^___________________________________________________________________\n\n^This ^is ^a ^bot, ^and ^this ^action ^was ^performed ^automatically.\n\n^Please ^contact ^[\/u\/aparker314159](https://reddit.com/user/aparker314159) ^for ^any ^questions ^or ^comments."
+    yield "#ALL ABOARD THE SWITCH HYPE TRAIN!\n#[CHOO CHOO!](http://imgur.com/y9igYes)\n" + str(onboard) + " people onboard the hype train!\n\n^___________________________________________________________________\n\n^This ^is ^a ^bot, ^and ^this ^action ^was ^performed ^automatically.\n\n^Please ^contact ^[\/u\/aparker314159](https://reddit.com/user/aparker314159) ^for ^any ^questions ^or ^comments."
 
 while True:
     subreddit = r.get_subreddit(subname)
@@ -18,7 +18,7 @@ while True:
     for c in comments:
         if c in finished:
             break
-        if "hype train" in (c.body).lower():
+        if "hype train" in (c.body).lower() and c.author.name != "testbotaparker314159":
             onboard = onboard + 1
             message = next(genmessage(onboard))
             c.reply(message)
